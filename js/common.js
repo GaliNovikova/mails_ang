403a535c64ea75f4cd6c574709c76c89688ae9d9
Galusya <gaya1510@gmail.com>
2016-03-10 19:04:05 +0200

    moving to components

--- js/common.js
+++ js/common.js
@@ -1,4 +1,79 @@
 $( document ).ready(function(){
-
+	
 
 });
+
+angular.module('moduleApp', [])
+      .directive('mailBoxes', function(){
+        return {
+          scope: true,
+          restrict: 'AE',
+          link: function(scope) {
+            scope.mailBoxesList = [
+              {
+                id: 1,
+                name: 'Inbox'
+              },
+              {
+                id:2,
+                name: 'Sent mail'
+              }
+              ];
+            scope.currentBoxId = scope.mailBoxesList[0].id;
+            scope.openBox = function(id){
+              scope.currentBoxId = id;
+            }
+          },
+          templateUrl : 'mail-boxes-tpl.html'
+          
+        };
+      })
+      .directive('mailList', function(){
+        return {
+          scope: true,
+          restrict: 'EA',
+          templateUrl : 'mail-list-tpl.html',
+          link: function(scope) {
+            scope.mailsList = [
+              {
+                "id": 0,
+                "author": "Иванов Иван",
+                "date": "2015-10-10",
+                "from": "ivanov@mail.ru",
+                "text": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+                "boxId": 1,
+                "subject":"Dear All"
+              },
+              {
+                "id": 1,
+                "author": "Иванов Петр",
+                "date": "2015-10-10",
+                "from": "ivanov@mail.ru",
+                "text": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+                "boxId": 2,
+                "subject":"Dear All"
+              },
+              {
+                "id": 2,
+                "author": "Иванов Николай",
+                "date": "2015-10-10",
+                "from": "ivanov@mail.ru",
+                "text": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+                "boxId": 1,
+                "subject":"Dear All"
+              },
+              {
+                "id": 3,
+                "author": "Петров Николай",
+                "date": "2015-10-10",
+                "from": "ivanov@mail.ru",
+                "text": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+                "boxId": 1,
+                "subject":"Dear All"
+              }
+              
+              ];
+          }
+        }
+      });
+
